6f66b3e04e9b9e90af766efb3a9433aec884ccfa
Daniel Bretón <daniel.breton@zivautomation.com>
2020-05-16 10:58:38 +0200

    First Makefile and hello world

---
+++ include/hello.h
@@ -0,0 +1,6 @@
+#ifndef HELLO_H_INCLUDED
+#define HELLO_H_INCLUDED
+
+#define HELLO "hola"
+
+#endif /* HELLO_H_INCLUDED */
---
+++ source/hello.cpp
@@ -0,0 +1,7 @@
+#include <hello.h>
+#include <stdio.h>
+
+int main()
+{
+	printf(HELLO"\n");
+}
